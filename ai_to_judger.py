93d0a6985e45c83c4b02673b2bcff58419766f1c
刘昕雨 <2023012166@secoder.net>
2024-10-30 16:32:29 +0800

    ai_to_judger

---
+++ ai_to_judger.py
@@ -0,0 +1,30 @@
+import json
+from utils.utils import write_to_judger
+
+def create_message(operation: int) -> str:
+    message = {
+        "role": 0,
+        "operation": operation
+    }
+    return json.dumps(message)
+
+# use functions below to send your operation to judger
+def stay(): 
+    msg = create_message(0)
+    write_to_judger(msg)
+
+def up():
+    msg = create_message(1)
+    write_to_judger(msg)
+
+def left():
+    msg = create_message(2)
+    write_to_judger(msg)
+
+def down():
+    msg = create_message(3)
+    write_to_judger(msg)
+
+def right():
+    msg = create_message(4)
+    write_to_judger(msg)
