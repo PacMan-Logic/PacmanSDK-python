9cbb6ddd721b9dafeaad224f14961367d41a5b59
zyc <1455854619@qq.com>
2024-12-02 13:55:37 +0800

    feat: render board for ai

--- ai.py
+++ ai.py
@@ -1,30 +1,35 @@
-from core.gamedata import GameState
+from core.gamedata import *
 
 
-def ai(game_state: GameState) -> list[int]:
+def distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+def example_pacman(game_state: GameState) -> list[int]:
     import numpy as np
 
     ghost_pos = game_state.ghosts_pos
     pacman_pos = game_state.pacman_pos
 
-    vector1 = np.array(pacman_pos) - np.array(ghost_pos[0])
-    standardize1 = vector1 / np.linalg.norm(vector1)
+    possible_next_blocks = []
+    if game_state.board[pacman_pos[0] + 1][pacman_pos[1]] != Space.WALL.value:
+        possible_next_blocks.append([pacman_pos[0] + 1, pacman_pos[1], Move.UP.value])
+    if game_state.board[pacman_pos[0] - 1][pacman_pos[1]] != Space.WALL.value:
+        possible_next_blocks.append([pacman_pos[0] - 1, pacman_pos[1], Move.DOWN.value])
+    if game_state.board[pacman_pos[0]][pacman_pos[1] + 1] != Space.WALL.value:
+        possible_next_blocks.append(
+            [pacman_pos[0], pacman_pos[1] + 1, Move.RIGHT.value]
+        )
+    if game_state.board[pacman_pos[0]][pacman_pos[1] - 1] != Space.WALL.value:
+        possible_next_blocks.append([pacman_pos[0], pacman_pos[1] - 1, Move.LEFT.value])
 
-    vector2 = np.array(pacman_pos) - np.array(ghost_pos[1])
-    standardize2 = vector2 / np.linalg.norm(vector2)
+    res = max(
+        possible_next_blocks,
+        key=lambda x: distance(x, ghost_pos[0])
+        + distance(x, ghost_pos[1])
+        + distance(x, ghost_pos[2]),
+    )
+    return [res[2]]
 
-    vector3 = np.array(pacman_pos) - np.array(ghost_pos[2])
-    standardize3 = vector3 / np.linalg.norm(vector3)
 
-    direction = standardize1 + standardize2 + standardize3
-
-    if abs(direction[0]) > abs(direction[1]):
-        if direction[0] > 0:
-            return [4]
-        else:
-            return [2]
-    else:
-        if direction[1] > 0:
-            return [1]
-        else:
-            return [3]
+ai_func = example_pacman
